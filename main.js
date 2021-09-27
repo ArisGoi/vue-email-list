9252decbda63602baddb1939917169fd846a22fb
ArisGoi <aris.goi97@gmail.com>
2021-09-27 16:10:06 +0200

    Add caricamento mail

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const app = new Vue({
 
     },
     mounted(){
-        for(let i=0; i<10; i++){
+        for(let i=0; i<30; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
                 this.mailList.push(response.data.response);
