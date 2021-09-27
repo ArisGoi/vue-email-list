098ae418e0947b774447ccaa1c2533b6a788227d
ArisGoi <aris.goi97@gmail.com>
2021-09-27 14:05:41 +0200

    add axios.get*10

--- main.js
+++ main.js
@@ -15,6 +15,12 @@ const app = new Vue({
         
     },
     mounted(){
-        
+        for(let i=0; i<10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // handle success
+                console.log(response.data);
+            })
+        }
     }
 });
